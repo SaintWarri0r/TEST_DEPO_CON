8678080f645a5ef3be78fab0fa049ee2b5f46ae4
SaintWarri0r <Troll25Rus@gmail.com>
2023-12-21 22:48:36 +0300

    Added func of showing comp_list

--- Test_DEPO_con/Program.cs
+++ Test_DEPO_con/Program.cs
@@ -17,8 +17,10 @@ public class Employee
     public string Employee_Passport_Series { get; set; }
     public string Employee_Passport_Number { get; set; }
 }
+
 class Con_Test_DEPO
 {
+    
     public static void Main()
     {
 
@@ -39,6 +41,7 @@ class Con_Test_DEPO
         Console.WriteLine($"\tВерсия сервера: {connection.ServerVersion}");
         Console.WriteLine($"\tСостояние: {connection.State}");
         Console.WriteLine($"\tWorkstationld: {connection.WorkstationId}");
+        connection.Close();
         //-------------------------------------------------------------------------------------------------------------
 
         Console.WriteLine("\n\nWhat did you want to do?\n------------------------------------------------------\n" +
@@ -49,12 +52,34 @@ class Con_Test_DEPO
 
         while (true)
         {
-            string presed_key = Console.ReadLine();
-            switch (presed_key)
+            
+            switch (Console.ReadLine())
             {
                 case "1":
+                    connection.Open();
+                    string Show_Company_List = "SELECT Company_Name FROM Company";
+                    SqlCommand Show_Company_command = new SqlCommand(Show_Company_List,connection);
+                    SqlDataReader Show_Company_reader = Show_Company_command.ExecuteReader();
+                    foreach (var row in Show_Company_reader)
+                    {
+                        string Company_NAME = Show_Company_reader.GetValue(0).ToString();
+                        Console.WriteLine(Company_NAME);
+                    }
+                    // определяем выполняемую команду
+                    connection.Close();
                     break;
                 case "2":
+                    connection.Open();
+                    string Show_Employee_List = "SELECT * FROM Employee";
+                    SqlCommand Show_Employee_command = new SqlCommand(Show_Employee_List, connection);
+                    SqlDataReader Show_Employee_reader = Show_Employee_command.ExecuteReader();
+                    foreach (var row in Show_Employee_reader)
+                    {
+                        string Company_NAME = Show_Employee_reader.GetValue(0).ToString();
+                        Console.WriteLine(Company_NAME);
+                    }
+                    // определяем выполняемую команду
+                    connection.Close();
                     break;
             }
 
